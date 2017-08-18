671198f95b23475552cf5214074cf2f9d0be097c
Danica <danica@DanicadeMacBook-Pro.local>
2017-08-18 17:53:26 +0800

    no message

--- cal.js
+++ cal.js
@@ -15,30 +15,37 @@ $(document).ready(function(){
       $("#swr_level").append("<option value='"+level[o].split(",")[1]+"'>"+level[o].split(",")[0]+"</option>");
       $("#tar_swr_level").append("<option value='"+level[o].split(",")[1]+"'>"+level[o].split(",")[0]+"</option>");
     };
+    calExp();
   });
   
   $("#swr_level").change(function(){
     var diff = $('#tar_swr_level').val() - $('#swr_level').val();
     $("#req_exp").text(toThousands(diff));
+    calExp();
   });
   
   $("#tar_swr_level").change(function(){
     var diff = $('#tar_swr_level').val() - $('#swr_level').val();
     $("#req_exp").text(toThousands(diff));
+    calExp();
   });
   
   $("#map").change(function(){
-    var exp = exp_master[$('#map').val()];
-    $("#nor_exp").text(exp.nor_exp);
-    $("#ave_exp").text(exp.ave_exp);
-    $("#boss_exp").text(exp.boss_exp);
-    var base = $('#tar_swr_level').val() - $('#swr_level').val();
-    $("#r1").text(Math.ceil( base / (exp.ave_exp*3.6)));
-    $("#r2").text(Math.ceil( base / (exp.ave_exp*2.4)));
-    $("#r3").text(Math.ceil( base / (exp.ave_exp*1.8)));
-    $("#r4").text(Math.ceil( base / (exp.ave_exp*1.2)));
+    calExp();
   });
 });
+
+function calExp(){
+  var exp = exp_master[$('#map').val()];
+  $("#nor_exp").text(exp.nor_exp);
+  $("#ave_exp").text(exp.ave_exp);
+  $("#boss_exp").text(exp.boss_exp);
+  var base = $('#tar_swr_level').val() - $('#swr_level').val();
+  $("#r1").text(Math.ceil( base / (exp.ave_exp*3.6)));
+  $("#r2").text(Math.ceil( base / (exp.ave_exp*2.4)));
+  $("#r3").text(Math.ceil( base / (exp.ave_exp*1.8)));
+  $("#r4").text(Math.ceil( base / (exp.ave_exp*1.2)));
+};
 
 function toThousands(num) {
     return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
